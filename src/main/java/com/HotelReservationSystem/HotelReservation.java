04f10a0c2846fb193a165077da85a5e39856663a
aakash2809 <aakashrajak289@gmail.com>
2020-12-05 09:20:06 +0530

    initiate the projec with welcom messsage

---
+++ src/main/java/com/HotelReservationSystem/HotelReservation.java
@@ -0,0 +1,10 @@
+package com.HotelReservationSystem;
+
+public class HotelReservation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcom to Hotel Reservation System");
+
+	}
+
+}
